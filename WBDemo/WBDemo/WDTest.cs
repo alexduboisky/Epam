a4dda98fd2865e4bd67efa7b62c7089dee617d9c
alexduboisky <alexduboisky@yandex.ru>
2017-11-12 21:18:45 +0300

    Change

--- WBDemo/WBDemo/WDTest.cs
+++ WBDemo/WBDemo/WDTest.cs
@@ -6,29 +6,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//"//a[text()='Sign in']"
+
 namespace WBDemo
 {
     [TestFixture]
     public class WDTest
     {
+        const string login = "testautomationuser";
+        const string password = "Time4Death!";
+
         [Test]
         public void OneCanOpenGoogle()
         {
             IWebDriver Chrome = new ChromeDriver();
+            Chrome.Manage().Window.Maximize();
             Chrome.Navigate().GoToUrl("https://github.com/");
-           
-            IWebElement SignInGitHub = Chrome.FindElement(By.XPath("/ html / body / div[1] / header / div / div[2] / div / span / div / a[1]"));
+
+            IWebElement SignInGitHub = Chrome.FindElement(By.XPath("//a[text()='Sign in']"));
             SignInGitHub.Click();
 
-            IWebElement InputLogin = Chrome.FindElement(By.XPath("//*[@id='login_field']"));
-            InputLogin.SendKeys("alexduboisky");
+            IWebElement InputLogin = Chrome.FindElement(By.XPath("//*[@name='login']"));
+            InputLogin.SendKeys(login);
 
-            IWebElement InputPassword = Chrome.FindElement(By.XPath("//*[@id='password']"));
-            InputPassword.SendKeys("8244919Ad");
+            IWebElement InputPassword = Chrome.FindElement(By.XPath("//*[@name='password']"));
+            InputPassword.SendKeys(password);
 
-            IWebElement SignInButton = Chrome.FindElement(By.XPath("//*[@id='login']/form/div[4]/input[3]"));
+            IWebElement SignInButton = Chrome.FindElement(By.XPath("//*[@name='commit']"));
             SignInButton.Click();
+
+            String user = Chrome.FindElement(By.XPath("//meta[@name='user-login']")).GetAttribute("content");
+            Assert.AreEqual(user, login);
+
+            Chrome.Quit();
         }
     }
 }
